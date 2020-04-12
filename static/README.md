75111e3f519a7bef11c23efd6d561b29da43cb68
Stephen Sherratt <stephen@sherra.tt>
2020-04-12 19:02:59 +1000

    Add static crate

---
+++ static/README.md
@@ -0,0 +1,4 @@
+# general\_storage\_static
+
+[![Version](https://img.shields.io/crates/v/general_storage_static.svg)](https://crates.io/crates/general_storage_static)
+[![Documentation](https://docs.rs/general_storage_static/badge.svg)](https://docs.rs/general_storage_static)
---
+++ static/src/lib.rs
@@ -0,0 +1,312 @@
+pub use general_storage::*;
+
+#[cfg(all(feature = "general_storage_file", feature = "general_storage_web"))]
+compile_error!("Choose exactly one out of: general_storage_file, general_storage_web");
+
+#[cfg(not(any(feature = "general_storage_file", feature = "general_storage_web")))]
+mod implementation {
+    use super::*;
+    pub mod backend {}
+    pub struct StaticStorage(());
+
+    impl StaticStorage {
+        pub fn exists<K>(&self, _key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        pub fn clear(&mut self) {
+            unimplemented!()
+        }
+
+        pub fn remove<K>(&mut self, _key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        pub fn load_raw<K>(&self, _key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        pub fn store_raw<K, V>(&mut self, _key: K, _value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            unimplemented!()
+        }
+        pub fn load<K, T, F>(
+            &self,
+            _key: K,
+            _format: F,
+        ) -> Result<T, LoadError<F::DeserializeError>>
+        where
+            K: AsRef<str>,
+            T: DeserializeOwned,
+            F: StorageFormat,
+        {
+            unimplemented!()
+        }
+        pub fn store<K, T, F>(
+            &mut self,
+            _key: K,
+            _value: &T,
+            _format: F,
+        ) -> Result<(), StoreError<F::SerializeError>>
+        where
+            K: AsRef<str>,
+            T: Serialize,
+            F: StorageFormat,
+        {
+            unimplemented!()
+        }
+    }
+
+    impl Storage for StaticStorage {
+        fn exists<K>(&self, _key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        fn clear(&mut self) {
+            unimplemented!()
+        }
+
+        fn remove<K>(&mut self, _key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        fn load_raw<K>(&self, _key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            unimplemented!()
+        }
+
+        fn store_raw<K, V>(&mut self, _key: K, _value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            unimplemented!()
+        }
+    }
+}
+
+#[cfg(feature = "general_storage_file")]
+mod implementation {
+    use super::*;
+    use backend::FileStorage;
+    pub use general_storage_file as backend;
+    pub struct StaticStorage(FileStorage);
+
+    impl StaticStorage {
+        pub fn new(inner: FileStorage) -> Self {
+            Self(inner)
+        }
+        pub fn exists<K>(&self, key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            self.0.exists(key)
+        }
+
+        pub fn clear(&mut self) {
+            self.0.clear()
+        }
+
+        pub fn remove<K>(&mut self, key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.remove(key)
+        }
+
+        pub fn load_raw<K>(&self, key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.load_raw(key)
+        }
+
+        pub fn store_raw<K, V>(&mut self, key: K, value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            self.0.store_raw(key, value)
+        }
+        pub fn load<K, T, F>(&self, key: K, format: F) -> Result<T, LoadError<F::DeserializeError>>
+        where
+            K: AsRef<str>,
+            T: DeserializeOwned,
+            F: StorageFormat,
+        {
+            self.0.load(key, format)
+        }
+        pub fn store<K, T, F>(
+            &mut self,
+            key: K,
+            value: &T,
+            format: F,
+        ) -> Result<(), StoreError<F::SerializeError>>
+        where
+            K: AsRef<str>,
+            T: Serialize,
+            F: StorageFormat,
+        {
+            self.0.store(key, value, format)
+        }
+    }
+
+    impl Storage for StaticStorage {
+        fn exists<K>(&self, key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            self.0.exists(key)
+        }
+
+        fn clear(&mut self) {
+            self.0.clear()
+        }
+
+        fn remove<K>(&mut self, key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.remove(key)
+        }
+
+        fn load_raw<K>(&self, key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.load_raw(key)
+        }
+
+        fn store_raw<K, V>(&mut self, key: K, value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            self.0.store_raw(key, value)
+        }
+    }
+}
+
+#[cfg(feature = "general_storage_web")]
+mod implementation {
+    use super::*;
+    use backend::LocalStorage;
+    pub use general_storage_web as backend;
+    pub struct StaticStorage(LocalStorage);
+
+    impl StaticStorage {
+        pub fn new(inner: LocalStorage) -> Self {
+            Self(inner)
+        }
+        pub fn exists<K>(&self, key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            self.0.exists(key)
+        }
+
+        pub fn clear(&mut self) {
+            self.0.clear()
+        }
+
+        pub fn remove<K>(&mut self, key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.remove(key)
+        }
+
+        pub fn load_raw<K>(&self, key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.load_raw(key)
+        }
+
+        pub fn store_raw<K, V>(&mut self, key: K, value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            self.0.store_raw(key, value)
+        }
+        pub fn load<K, T, F>(&self, key: K, format: F) -> Result<T, LoadError<F::DeserializeError>>
+        where
+            K: AsRef<str>,
+            T: DeserializeOwned,
+            F: StorageFormat,
+        {
+            self.0.load(key, format)
+        }
+        pub fn store<K, T, F>(
+            &mut self,
+            key: K,
+            value: &T,
+            format: F,
+        ) -> Result<(), StoreError<F::SerializeError>>
+        where
+            K: AsRef<str>,
+            T: Serialize,
+            F: StorageFormat,
+        {
+            self.0.store(key, value, format)
+        }
+    }
+
+    impl Storage for StaticStorage {
+        fn exists<K>(&self, key: K) -> bool
+        where
+            K: AsRef<str>,
+        {
+            self.0.exists(key)
+        }
+
+        fn clear(&mut self) {
+            self.0.clear()
+        }
+
+        fn remove<K>(&mut self, key: K) -> Result<(), RemoveError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.remove(key)
+        }
+
+        fn load_raw<K>(&self, key: K) -> Result<Vec<u8>, LoadRawError>
+        where
+            K: AsRef<str>,
+        {
+            self.0.load_raw(key)
+        }
+
+        fn store_raw<K, V>(&mut self, key: K, value: V) -> Result<(), StoreRawError>
+        where
+            K: AsRef<str>,
+            V: AsRef<[u8]>,
+        {
+            self.0.store_raw(key, value)
+        }
+    }
+}
+
+pub use implementation::*;
